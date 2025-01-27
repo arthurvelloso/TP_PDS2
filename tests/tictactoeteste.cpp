4fa4d5ae0c121b60ecd785ecbe2edfdcc33dfa89
marianaordones <mariordones2005@gmail.com>
2025-01-27 19:05:29 -0300

    Update tictactoeteste.cpp

--- tests/tictactoeteste.cpp
+++ tests/tictactoeteste.cpp
@@ -2,6 +2,22 @@
 #include <doctest/doctest.h>
 #include "tictactoe.hpp" // Inclua o arquivo de cabeçalho da classe
 
+TEST_CASE("Testando o construtor de Tictactoe") {
+    Tictactoe game;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            CHECK(game.get_board(i, j) == 0);
+        }
+    }
+
+    // Verifica o jogador inicial
+    CHECK(game.get_current_player() == 1);
+
+    // Verifica se o jogo não terminou inicialmente
+    CHECK(game.get_is_game_ended() == false);
+}
+
 TEST_CASE("Tictactoe - Teste do método is_move_valid") {
     Tictactoe jogo;
 
