e0a092b6e0d6abe92704d4f2282c0631a1156bdb
Arthur Velloso <164116095+arthurvelloso@users.noreply.github.com>
2025-01-27 16:14:19 -0300

    Tratamento de exceções connect4.cpp

--- src/connect4.cpp
+++ src/connect4.cpp
@@ -1,5 +1,7 @@
 #include "connect4.hpp"
 #include <iostream>
+#include <limits>
+#include <stdexcept>
 
 using namespace std;
 
@@ -36,23 +38,53 @@ void Connect4::print_board() {
 }
 
 // Realizar jogada
-void Connect4::read_move() {
+void Connect4:: read_move(){
     int move;
     bool valid_input = false;
-    while (!valid_input) {
-        cout << "It's " << (current_player == 1 ? "black" : "white") << "'s turn!" << endl;
-        cout << "Enter the column you want to play (0-6)" << endl;
+
+    
+    try {  
+        cout << "Player " << (current_player == 1 ? "1 (X)" : "2 (O)") << ", enter the column you want to play (0-6): "; 
         cin >> move;
-        if (is_move_valid(move, 0)) {
-            last_move = move;
-            board[column_height[move]][move] = current_player;
-            test_win_condition();
-            column_height[move]--;
-            current_player = -current_player;
-            valid_input = true;
-        } else {
-            cout << "That's not a valid move! Enter a column between 0 and 6 that's not filled" << endl;
+        cout << endl;     
+
+        // Verifica se a entrada é inválida (não é um número inteiro)
+        if (cin.fail()) {
+            cin.clear(); // Limpa o estado de erro do cin
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Descartar a entrada inválida
+            throw std::invalid_argument("Invalid input! Please, your number must be a number");
         }
+
+        // Verifica se a posição está fora dos limites válidos
+        if (move < 0 || move > 6) {
+            throw std::out_of_range("Invalid! Please, type a number between 1 e 9.");
+        }
+
+        if (!is_move_valid(0, move)) {
+            throw std::invalid_argument("Occupped position, try again");
+        }
+
+        last_move = move;
+        board[column_height[move]][move] = current_player;
+        test_win_condition();
+        column_height[move]--;
+        current_player = -current_player;
+        valid_input = true;
+    }
+    catch (const std::out_of_range& e) {
+        // Trata a exceção de posição fora dos limites
+        cout << e.what() << endl;
+        read_move(); // Solicita outra jogada
+
+    } catch (const std::invalid_argument& e) {
+        // Trata a exceção de posição ocupada
+        cout << e.what() << endl;
+        read_move(); // Solicita outra jogada
+    }
+    catch (...) {
+        // Tratamento genérico para exceções inesperadas
+        cout << "Inesperated error. Try again a new play." << endl;
+        read_move(); // Solicita outra jogada
     }
 }
 
